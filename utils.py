e9e584be9884be9bf0e250d85d94f1a380ab0b59
fensta <fensta.git@gmail.com>
2018-11-06 19:27:39 +0300

    - create lookup matrix for svdm

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import math
-from collections import Counter
+from collections import Counter, defaultdict
 import os
+import itertools
 
 import numpy as np
 import pandas as pd
@@ -13,9 +14,10 @@ NOMINAL = 0
 NUMERIC = 1
 TYPE_MAPPING = {""}
 DATA_TYPES = []
+CONDITIONAL = "Conditional"
 
 
-def read_dataset(src, excluded=[], skip_rows=0, na_values=[], normalize=True):
+def read_dataset(src, excluded=[], skip_rows=0, na_values=[], normalize=False, class_index=-1):
     """
     Reads in a dataset in csv format and stores it in a dataFrame.
 
@@ -26,6 +28,7 @@ def read_dataset(src, excluded=[], skip_rows=0, na_values=[], normalize=True):
     skip_rows: int - number of rows to skip at the beginning of <src>
     na_values: list of str - values encoding missing values - these are represented by NaN
     normalize: bool - True if numeric values should be in range between 0 and 1
+    class_index: int - 0-based index where class label is stored in dataset. -1 = last column
 
     Returns
     -------
@@ -34,14 +37,26 @@ def read_dataset(src, excluded=[], skip_rows=0, na_values=[], normalize=True):
 
     """
     df = pd.read_csv(src, skiprows=skip_rows, na_values=na_values)
-    # TODO: normalize numeric features to the range (0-1)
-    if normalize:
-       df = normalize(df)
+    lookup = {}
+    # Convert fancy index to regular index - otherwise the loop below won't skip the column with class labels
+    if class_index == -1:
+        class_index = len(df.columns)
+    # Create lookup matrix for nominal features for SVDM + normalize numerical features columnwise
+    for i, _ in enumerate(df):
+        if i == class_index:
+            return
+        col = df.iloc[:, i]
+        if is_numeric_dtype(col):
+            if normalize:
+                df.iloc[:, i] = normalize_series(col)
+        else:
+            lookup[i] = create_svdm_lookup_column(df, col, class_index)
+
     return df
 
 
-def normalize(df):
-    """Normalize numeric features"""
+def normalize_dataframe(df):
+    """Normalize numeric features (=columns) using min-max normalization"""
     for col_idx, _ in enumerate(df):
         col = df.iloc[:, col_idx]
         if is_numeric_dtype(col):
@@ -49,6 +64,57 @@ def normalize(df):
             max_val = col.max()
             df.iloc[:, col_idx] = (col - min_val) / (max_val - min_val)
     return df
+
+
+def normalize_series(col):
+    """Normalizes a given series assuming it's data type is numeric"""
+    if is_numeric_dtype(col):
+        min_val = col.min()
+        max_val = col.max()
+        col = (col - min_val) / (max_val - min_val)
+    return col
+
+
+def create_svdm_lookup_column(df, coli, class_idx):
+    """
+    Create sparse lookup table for the feature representing the current column i.
+
+    N(xi), N(yi), N(xi, Kj), N(yi, Kj), is stored per nominal feature, where N(xi) and N(yi) are the numbers of
+    examples for which the value on i-th feature (coli) is equal to xi and yi respectively, N(xi , Kj) and N(yi,
+    Kj) are the numbers of examples from the decision class Kj , which belong to N(xi) and N(yi), respectively
+
+    Parameters
+    ----------
+    df: pd.DataFrame - dataset.
+    coli: pd.Series - i-th column (= feature) of the dataset
+    class_idx: int - index of class label in <df>.
+
+    Returns
+    -------
+    dict - sparse dictionary holding the non-zero counts of all values of <coli> with the class labels
+
+    """
+    # classes = df.iloc[:, class_idx].unique()
+    c = {}
+    # print("Kj:\n", classes)
+    # print(coli)
+    nxiyi = Counter(coli.values)
+    c.update(nxiyi)
+    c[CONDITIONAL] = {}
+    # print("N(xi/yi)\n", nxiyi)
+    unique_xiyi = nxiyi.keys()
+    # Create all pairwise combinations of two values
+    combinations = itertools.combinations(unique_xiyi, 2)
+    # for i in range(0, len(unique_xiyi), 2):
+    for combo in combinations:
+        for val in combo:
+            # print("current value:\n", val)
+            rows_with_val = df.loc[coli == val]
+            # print("rows with value:\n", rows_with_val)
+            nxiyikj = Counter(rows_with_val.iloc[:, class_idx].values)
+            # print("counts:\n", nxiyikj)
+            c[CONDITIONAL][val] = nxiyikj
+    return c
 
 
 def hvdm(xi, yi, counts):
@@ -132,7 +198,12 @@ def di(f1, f2):
         print("NaN(s) in di()")
         return 1.
     dist = 0.
-
+    for col_idx, _ in enumerate(df):
+        col = df.iloc[:, col_idx]
+        if is_numeric_dtype(col):
+            min_val = col.min()
+            max_val = col.max()
+            df.iloc[:, col_idx] = (col - min_val) / (max_val - min_val)
     return dist
 
 
