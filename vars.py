eae778212cd94b76c6e8d34d92e3e7834d1dadfa
fensta <fensta.git@gmail.com>
2018-11-21 18:25:13 +0300

    - moved global variables into an extra file

---
+++ vars.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+MISSING_VAL = np.nan
+NOMINAL = 0
+NUMERIC = 1
+TYPE_MAPPING = {""}
+DATA_TYPES = []
+CONDITIONAL = "Conditional"
+CLASSES = []
+SAFE = "safe"
+NOISY = "noisy"
+BORDERLINE = "borderline"
+TAG = "tag"
+DISTANCE_MATRIX = {}
+TP = "tp"
+TN = "tn"
+FP = "fp"
+FN = "fn"
+# {example ei: seed for rule ri}
+seed_mapping = {}
+# {rule ri: set(closest rule for examples ei, ej)]}
+predicted_examples = {}
+# {example ei: predicted by rule ri}
+closest_rule = {}
+conf_matrix = {TP: set(), FP: set(), TN: set(), FN: {}}
+latest_id = 0
