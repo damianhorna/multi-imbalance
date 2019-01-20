3e0b35e6ed92390d42240c938cb2bbb45da9ae17
fensta <fensta.git@gmail.com>
2019-01-20 20:58:50 +0300

    - compute_f1_for_predictions() now also returns the confusion matrix itself besides the F1 score

--- test_compute_f1_for_predictions.py
+++ test_compute_f1_for_predictions.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 import pandas as pd
 
 from scripts.utils import compute_f1_for_predictions, Predictions
+import scripts.vars as my_vars
 
 
 class TestComputeF1ForPredictions(TestCase):
@@ -19,5 +20,7 @@ class TestComputeF1ForPredictions(TestCase):
                                 "Class": ["apple", "apple", "banana", "banana", "banana", "banana"]})
         class_col_name = "Class"
         minority_class = "banana"
-        f1 = compute_f1_for_predictions(dataset, predictions, class_col_name, minority_class)
+        f1, conf_matrix = compute_f1_for_predictions(dataset, predictions, class_col_name, minority_class)
+        correct_conf_matrix = {my_vars.TP: {3, 4}, my_vars.FP: {0, 1},  my_vars.FN: {2, 5}, my_vars.TN: set()}
+        self.assertTrue(conf_matrix == correct_conf_matrix)
         self.assertTrue(f1 == 0.5)
