bfe713a9e67ddd04a0676014edb53a19a93b6c21
plutasnyy <kamil.plucinski97@gmail.com>
2019-09-16 11:09:44 -0700

    updated PCA in SOUP example

--- multi_imbalance/utils/data.py
+++ multi_imbalance/utils/data.py
@@ -5,21 +5,17 @@ import pandas as pd
 
 def construct_flat_2pc_df(X: Array[float], y: Array) -> pd.DataFrame:
     """
-    This function uses PCA to reduce X dimensions and creates dataframe with 2 principal component and labels
-    for this data. In case of X with 2 dimension it skips the PCA part
+    This function takes two dimensional X and one dimensional y arrays, concatenates and returns them as data frame
 
     Parameters
     ----------
-    X multi dimensional numpy array (at least 2 dimensions)
+    X two dimensional numpy array
     y one dimensional numpy array with labels
 
-    Returns dataframe with 3 columns x1 x2 and y and with number of rows equal to number of rows in X
+    Returns data frame with 3 columns x1 x2 and y and with number of rows equal to number of rows in X
     -------
 
     """
-    if X.shape[1] > 2:
-        pca = PCA(n_components=2)
-        X = pca.fit_transform(X)
 
     y = pd.DataFrame({'y': y})
     X_df = pd.DataFrame(data=X, columns=['x1', 'x2'])
