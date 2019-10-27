b508021560d08a81a930842cfb211fdd45249089
jacekgry <jacek.grycza@student.put.poznan.pl>
2019-10-27 17:38:10 +0100

    oversampling for binary classifiers

--- multi_imbalance/ensemble/ovo.py
+++ multi_imbalance/ensemble/ovo.py
@@ -4,6 +4,7 @@ from sklearn.base import BaseEstimator
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.utils import check_random_state
 
 
 class OVO(BaseEstimator):
@@ -17,7 +18,7 @@ class OVO(BaseEstimator):
 
     """
 
-    def __init__(self, voting_strategy='max', binary_classifier='CART', n_neighbors=5):
+    def __init__(self, voting_strategy='max', binary_classifier='CART', n_neighbors=5, oversample_binary=None):
         """
         Parameters
         ----------
@@ -30,12 +31,15 @@ class OVO(BaseEstimator):
         * 'CART': Decision Tree Classifier,
         * 'KNN': K-Nearest Neighbors
         * 'NB' : Naive Bayes
+
+        n_neighbors: number of nearest neighbors in KNN, works only if binary_classifier=='KNN'
         """
         self.voting_strategy = voting_strategy
         self.binary_classifier = binary_classifier
         self.n_neighbors = n_neighbors
         self._binary_classifiers = []
         self._labels = np.array([])
+        self.oversample_binary = oversample_binary
 
     def fit(self, X, y):
         """
@@ -52,7 +56,6 @@ class OVO(BaseEstimator):
         num_of_classes = len(self._labels)
         self._binary_classifiers = [[self._get_classifier() for _ in range(n)] for n in
                                     range(0, num_of_classes)]
-
         self._learn_binary_classifiers(X, y)
         return self
 
@@ -89,6 +92,7 @@ class OVO(BaseEstimator):
                 first_class, second_class = self._labels[row], self._labels[col]
                 filtered_indices = [idx for idx in range(len(y)) if y[idx] in (first_class, second_class)]
                 X_filtered, y_filtered = X[filtered_indices], y[filtered_indices]
+                X_filtered, y_filtered = self._oversample(X_filtered, y_filtered, strategy=self.oversample_binary)
                 self._binary_classifiers[row][col].fit(X_filtered, y_filtered)
 
     def _get_classifier(self):
@@ -120,3 +124,35 @@ class OVO(BaseEstimator):
             for clf_2 in range(clf_1):
                 scores[self._labels.tolist().index(binary_outputs_matrix[clf_1][clf_2])] += 1
         return self._labels[np.argmax(scores)]
+
+    def _oversample(self, X, y, strategy=None):
+        allowed_oversampling = [None, 'random', 'SMOTE']
+        if strategy not in allowed_oversampling:
+            raise ValueError("Unknown matrix generation encoding: %s, expected to be one of %s."
+                             % (self.encoding, allowed_oversampling))
+        elif strategy is None:
+            return X, y
+        elif strategy == 'random':
+            return self._random_oversample(X, y)
+        elif strategy == 'SMOTE':
+            return self._smote_oversample(X, y)
+
+    def _random_oversample(self, X, y, random_state=0):
+        random_state = check_random_state(random_state)
+
+        values, counts = np.unique(y, return_counts=True)
+        max_cardinality = np.max(counts)
+        instances_to_be_added = max_cardinality - counts
+
+        for clazz, missing_examples in zip(values, instances_to_be_added):
+            y_clazz_indices = np.where(y == clazz)
+            X_clazz = X[y_clazz_indices]
+            for _ in range(missing_examples):
+                rand_idx = random_state.randint(0, X_clazz.shape[0])
+                y = np.append(y, clazz)
+                X = np.vstack([X, X_clazz[rand_idx]])
+
+        return X, y
+
+    def _smote_oversample(self, X, y):
+        pass
