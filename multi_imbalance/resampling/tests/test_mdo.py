fixed nan error during calculating mahalanobis distance variables, error was result as 0 in variance

multi_imbalance/resampling/tests/test_mdo.py
@@ -85,7 +85,7 @@ def test_choose_samples_when_correct(mdo_mock):
     V = np.array([7.53e-02, 5.91e-34])
     oversampling_rate = 2
     weights = [0.3, 0.7]
-    expected_result = np.array([[1.66857e-01, 3.3090e-17], [2.69258e-01, 3.4293e-17]])
+    expected_result = np.array([[1.179238e-01, 9.728434e-09], [1.902940e-01, 9.301928e-09]])
 
     S_temp = clf._MDO_oversampling(T, V, oversampling_rate, weights)
     assert_array_almost_equal(S_temp, expected_result)
@@ -100,3 +100,15 @@ def test_choose_samples_when_zero_samples_expected(mdo_mock):
 
     S_temp = clf._MDO_oversampling(T, V, oversampling_rate, weights)
     assert len(S_temp) == 0
+
+
+def test_zero_variance(mdo_mock):
+    clf = mdo_mock(X, list())
+    T = np.array([[-2.74e-01, -2.43e-17], [2.74e-01, 2.43e-17]])
+    V = np.array([0, 0])
+    oversampling_rate = 2
+    weights = [0.3, 0.7]
+    expected_result = np.array([[0.117924, 0.266956], [0.190294, 0.255253]])
+
+    S_temp = clf._MDO_oversampling(T, V, oversampling_rate, weights)
+    assert_array_almost_equal(S_temp, expected_result)