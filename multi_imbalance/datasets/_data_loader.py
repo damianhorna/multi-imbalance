be24927c4bf6caa3dfd8491dab73e83a7c809c21
jacekgry <jacek.grycza@student.put.poznan.pl>
2019-11-04 21:38:03 +0100

    datasets v3

--- multi_imbalance/datasets/_data_loader.py
+++ multi_imbalance/datasets/_data_loader.py
@@ -3,16 +3,7 @@
 The characteristics of the available datasets are presented in the table below.
 
  ID    Name             Repository     Class distribution                                            #S       #F
- 1     balance_scale    UCI            46.1 : 46.1 : 7.8                                             625      4
- 2     cmc              UCI            42.7 : 34.7 : 22.6                                            1,473    9
- 3     cleveland        UCI            53.9 : 18.2 : 11.8 : 11.8 : 4.4                               297      13
- 4     dermatology      UCI            31   : 19.8 : 16.8 : 13.4 : 13.4 : 5.6                        358      34
- 5     ecoli            UCI            42.6 : 22.9 : 15.5 : 10.4 : 6    : 1.5  : 0.6 : 0.6           336      7
- 6     glass            UCI            35.5 : 32.7 : 13.6 : 8    : 6.1  : 4.2                        214      9
- 7     hayes_roth       UCI            38.6 : 38.6 : 22.7                                            132      5
- 8     new_thyroid      UCI            69.8 : 16.3 : 14                                              215      5
- 9     winequailty_red  UCI            42.6 : 39.9 : 12.4 : 3.3  : 1.1 : 0.6                         1599     11
- 10    yeast            UCI            31.2 : 28.9 : 16.4 : 11   : 3.4 : 3 : 2.5 : 2 : 1.4 : 0.3    1,484    9
+
 
 """
 
@@ -30,11 +21,10 @@ PRE_FILENAME = 'x'
 POST_FILENAME = 'data.npz'
 DATA_HOME_BASIC = "./../../data/"
 
-MAP_NAME_ID_KEYS = [
-    'balance_scale', 'cleveland', 'cmc', 'dermatology',
-    'ecoli', 'glass', 'hayes_roth',
-    'new_thyroid', 'winequailty_red', 'yeast'
-]
+MAP_NAME_ID_KEYS = ['1czysty-cut', '2delikatne-cut', '3mocniej-cut', '4delikatne-bezover-cut',
+                    'balance-scale', 'cleveland', 'cleveland_v2', 'cmc', 'dermatology',
+                    'glass', 'hayes-roth', 'new_ecoli', 'new_led7digit', 'new_vehicle',
+                    'new_winequality-red', 'new_yeast', 'thyroid-newthyroid']
 
 MAP_NAME_ID = OrderedDict()
 MAP_ID_NAME = OrderedDict()
