92d8185feb19ce908b098c9068d8606f88d02528
jacekgry <jacek.grycza@student.put.poznan.pl>
2019-11-14 18:17:54 +0100

    allowed to apply oversampling only between minority and majority classes

--- multi_imbalance/ensemble/ovo.py
+++ multi_imbalance/ensemble/ovo.py
@@ -21,7 +21,8 @@ class OVO(BaseEstimator):
 
     """
 
-    def __init__(self, voting_strategy='max', binary_classifier='CART', n_neighbors=5, oversample_binary=None):
+    def __init__(self, voting_strategy='max', binary_classifier='CART', n_neighbors=5, oversample_binary=None,
+                 oversample_between='all'):
         """
         Parameters
         ----------
@@ -37,26 +38,34 @@ class OVO(BaseEstimator):
 
         n_neighbors: number of nearest neighbors in KNN, works only if binary_classifier=='KNN'
 
+        oversample_between: types of classes between which oversampling should be applied. Possible values:
+        * 'all' - oversampling between each pair of classes
+        * 'maj-min' - oversampling only between majority ad minority classes
+
         """
         self.voting_strategy = voting_strategy
         self.binary_classifier = binary_classifier
         self.n_neighbors = n_neighbors
+        self.oversample_binary = oversample_binary
+        self.oversample_between = oversample_between
         self._binary_classifiers = []
         self._labels = np.array([])
-        self.oversample_binary = oversample_binary
+        self._minority_classes = list()
 
-    def fit(self, X, y):
+    def fit(self, X, y, minority_classes):
         """
         Parameters
         ----------
         X: two dimensional numpy array (number of samples x number of features) with float numbers
         y: one dimensional numpy array with labels for rows in X
+        minority_classes: list of classes considered to be minority
 
         Returns
         -------
         self: object
         """
         self._labels = np.unique(y)
+        self._minority_classes = minority_classes
         num_of_classes = len(self._labels)
         self._binary_classifiers = [[self._get_classifier() for _ in range(n)] for n in
                                     range(0, num_of_classes)]
@@ -86,8 +95,8 @@ class OVO(BaseEstimator):
         binary_outputs_matrix = np.zeros((num_of_classes, num_of_classes))
         for class_idx1 in range(len(self._labels)):
             for class_idx2 in range(class_idx1):
-                binary_outputs_matrix[class_idx1][class_idx2] = self._binary_classifiers[class_idx1][
-                    class_idx2].predict([instance])
+                binary_outputs_matrix[class_idx1][class_idx2] = self._binary_classifiers[class_idx1][class_idx2] \
+                    .predict([instance])
         return binary_outputs_matrix
 
     def _learn_binary_classifiers(self, X, y):
@@ -96,7 +105,8 @@ class OVO(BaseEstimator):
                 first_class, second_class = self._labels[row], self._labels[col]
                 filtered_indices = [idx for idx in range(len(y)) if y[idx] in (first_class, second_class)]
                 X_filtered, y_filtered = X[filtered_indices], y[filtered_indices]
-                X_filtered, y_filtered = self._oversample(X_filtered, y_filtered, strategy=self.oversample_binary)
+                if self.should_perform_ovesampling(first_class, second_class):
+                    X_filtered, y_filtered = self._oversample(X_filtered, y_filtered, strategy=self.oversample_binary)
                 self._binary_classifiers[row][col].fit(X_filtered, y_filtered)
 
     def _get_classifier(self):
@@ -105,7 +115,7 @@ class OVO(BaseEstimator):
             raise ValueError("Unknown binary classifier: %s, expected to be one of %s."
                              % (self.binary_classifier, allowed_classifiers))
         elif self.binary_classifier == 'CART':
-            decision_tree_classifier = DecisionTreeClassifier()  # by default pruning is disabled
+            decision_tree_classifier = DecisionTreeClassifier()
             return decision_tree_classifier
         elif self.binary_classifier == 'NB':
             gnb = GaussianNB()
@@ -146,7 +156,18 @@ class OVO(BaseEstimator):
         if min(np.unique(y, return_counts=True)[1]) < 2:
             return GlobalCS().fit_transform(X, y)
 
-        n_neighbors = min(3, min(np.unique(y, return_counts=True)[1]) - 1)
+        n_neighbors = 3 if min(np.unique(y, return_counts=True)[1]) > 3 else 1
         smote = SMOTE(k_neighbors=n_neighbors)
         smote.fit(X, y)
         return smote.fit_resample(X, y)
+
+    def should_perform_ovesampling(self, first_class, second_class):
+        allowed_oversample_between = ['all', 'maj-min']
+        if self.oversample_between not in allowed_oversample_between:
+            raise ValueError("Unknown strategy for oversampling: %s, expected to be one of %s."
+                             % (self.oversample_between, allowed_oversample_between))
+        elif self.oversample_between == 'all':
+            return True
+        elif self.oversample_between == 'maj-min':
+            return (first_class in self._minority_classes and second_class not in self._minority_classes) or \
+                   (second_class in self._minority_classes and first_class not in self._minority_classes)
