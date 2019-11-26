55694be70d6abaf1a3032a0a1dd66b476fcf511d
plutasnyy <kamil.plucinski97@gmail.com>
2019-11-26 13:38:04 +0100

    added docs

--- multi_imbalance/resampling/SOUPBagging.py
+++ multi_imbalance/resampling/SOUPBagging.py
@@ -21,22 +21,54 @@ class SOUPBagging(object):
             else:
                 self.classifiers.append(KNeighborsClassifier())
 
-    def fit(self, X_train, y_train):
-        self.classes = np.unique(y_train)
+    def fit(self, X, y):
+        """
+
+        :param X: {array-like, sparse matrix} of shape = [n_samples, n_features] The training input samples.
+        :param y: array-like, shape = [n_samples]. The target values (class labels).
+        :return: self object
+        """
+        self.classes = np.unique(y)
         for clf in self.classifiers:
-            x_sampled, y_sampled = resample(X_train, y_train, stratify=y_train, random_state=self.random_state)
+            x_sampled, y_sampled = resample(X, y, stratify=y, random_state=self.random_state)
             x_resampled, y_resampled = SOUP().fit_transform(x_sampled, y_sampled)
             clf.fit(x_resampled, y_resampled)
+        return self
 
-    def predict(self, X_test):
-        n_samples = X_test.shape[0]
+    def predict(self, X):
+        """
+        Predict class for X. The predicted class of an input sample is computed as the class with the highest
+        sum of predicted probability.
+
+        :param X: {array-like, sparse matrix} of shape = [n_samples, n_features]. The training input samples.
+        :return: y : array of shape = [n_samples]. The predicted classes.
+        """
+        n_samples = X.shape[0]
         n_classes = self.classes.shape[0]
 
         results = np.zeros(shape=(self.n_classifiers, n_samples, n_classes))
 
         for i, clf in enumerate(self.classifiers):
-            results[i] = clf.predict_proba(X_test)
+            results[i] = clf.predict_proba(X)
 
         weights_sum = np.sum(results, axis=0)
         y_result = np.argmax(weights_sum, axis=1)
         return y_result
+
+    def predict_proba(self, X):
+        """
+        Predict class probabilities for X.
+
+        :param X:{array-like, sparse matrix} of shape = [n_samples, n_features]. The training input samples.
+        :return: p : array of shape = [n_samples, n_classes]. The class probabilities of the input samples.
+        """
+        n_samples = X.shape[0]
+        n_classes = self.classes.shape[0]
+
+        results = np.zeros(shape=(self.n_classifiers, n_samples, n_classes))
+
+        for i, clf in enumerate(self.classifiers):
+            results[i] = clf.predict_proba(X)
+
+        p = np.sum(results, axis=0)
+        return p
