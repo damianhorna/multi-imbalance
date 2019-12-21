f1765848cc83cedece74482ceb150c41d7bff214
hancia <hanna.klimczak@gmail.com>
2019-12-21 20:52:47 +0100

    Add underbagging, rework and documentation

--- multi_imbalance/ensemble/mrbbagging.py
+++ multi_imbalance/ensemble/mrbbagging.py
@@ -1,3 +1,4 @@
+from collections import Counter
 
 from scipy.stats import multinomial
 from sklearn.utils import resample
@@ -5,6 +6,23 @@ import numpy as np
 
 
 class MRBBagging(object):
+    """
+    Multi-class Roughly Balanced Bagging (MRBBagging) is a generalization of MRBBagging for adapting to multiple
+    minority classes.
+
+    Reference:
+    M. Lango, J. Stefanowski:
+    Multi-class and feature selection extensions of RoughlyBalanced Bagging for imbalanced data.
+    J. J Intell Inf Syst (2018) 50: 97
+
+    Methods:
+    ----------
+    fit(x, y, k, learning_algorithm, undersampling=True)
+        Build a MRBBagging ensemble of estimators from the training data.
+
+    predict(data)
+        Predict classes for examples in data.
+    """
     def __init__(self):
         self.classifiers, self.classes, self.classifier_classes = dict(), dict(), dict()
 
@@ -18,12 +36,31 @@ class MRBBagging(object):
             grouped_data[cl] = list(filter(lambda d: d[1] == cl, data))
         return classes, grouped_data
 
-    def fit(self, x, y, n, k, learning_algorithm):
+    def fit(self, x, y, la_list, undersampling=True):
+        """
+        Parameters
+        ----------
+        :param x:
+            Two dimensional numpy array (number of samples x number of features) with float numbers.
+        :param y:
+            One dimensional numpy array with labels for rows in X.
+        :param la_list:
+            List of classifiers.
+        :param undersampling:
+            Boolean value indicating the sampling method.
+        """
         assert len(x) == len(y), "Not enough labels"
-        assert learning_algorithm is not None, "Invalid learning algorithm"
+        assert la_list is not None, "Invalid learning algorithm"
         classes, grouped_data = self._group_data(x, y)
         prob = [1 / len(classes)] * len(classes)
-        for i in range(k):
+        self._set_classes_dict(classes)
+
+        n = len(x)
+        if undersampling:
+            q = Counter(y)
+            n = min(q.values()) * len(q)
+
+        for i in range(len(la_list)):
             samples_no = multinomial.rvs(n=n, p=prob)
             subset_x, subset_y = [], []
             for no, j in enumerate(classes):
@@ -32,15 +69,19 @@ class MRBBagging(object):
                 for sample in resample_class:
                     subset_x.append(sample[0])
                     subset_y.append(sample[1])
-            self.classifiers[i] = learning_algorithm[i].fit(subset_x, subset_y)
+            self.classifiers[i] = la_list[i].fit(subset_x, subset_y)
+
         return self
 
-    def _set_classes_dict(self):
-        self.classifier_classes = {key: value for (key, value) in enumerate(self.classifiers[0].classes_)}
+    def _set_classes_dict(self, classes):
+        classes = list(classes)
+        classes_dict = {}
+        for i, item in enumerate(classes):
+            classes_dict[i] = item
+        self.classifier_classes = classes_dict
 
     def _count_votes(self, data):
         voting_matrix = np.zeros((len(data), len(self.classes)))
-        self._set_classes_dict()
         for c in range(len(self.classifiers)):
             classes = self.classifiers[c].predict(data)
             probabilities = self.classifiers[c].predict_proba(data)
@@ -58,4 +99,10 @@ class MRBBagging(object):
         return selected_classes
 
     def predict(self, data):
+        """
+        Parameters
+        ----------
+        :param data:
+            Two dimensional numpy array (number of samples x number of features) with float numbers.
+        """
         return self._select_classes(data)
