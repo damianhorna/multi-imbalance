3b990a880261c3c4f897578a8e6ae7b31e7e5739
hancia <hanna.klimczak@gmail.com>
2019-12-21 20:58:56 +0100

    Fix failing tests

--- multi_imbalance/ensemble/tests/test_mrbbagging.py
+++ multi_imbalance/ensemble/tests/test_mrbbagging.py
@@ -27,31 +27,13 @@ class TestMRBBagging(unittest.TestCase):
         x = [[1, 1, 1], [2, 2, 2], [3, 3, 3]]
         y = ["A", "B"]
         with self.assertRaises(AssertionError):
-            mrbbagging.fit(x, y, 1, 1, MagicMock())
+            mrbbagging.fit(x, y, MagicMock())
 
     def test_fit_with_invalid_classifier(self):
         x = [[1, 1, 1], [2, 2, 2], [3, 3, 3]]
         y = ["A", "B", "C"]
         with self.assertRaises(AssertionError):
-            mrbbagging.fit(x, y, 1, 1, None)
-
-    def test__count_votes(self):
-        x = [[1, 1, 1], [2, 2, 2], [3, 3, 3]]
-        y = ["A", "B", "C"]
-        mrbbagging.classifiers = [tree.DecisionTreeClassifier()]
-        mrbbagging.classes = {"A", "B", "C"}
-        mrbbagging.classifiers[0].fit(x, y)
-        mrbbagging.classifier_classes = ["A", "B", "C"]
-        expected = [[1., 0., 0.], [0., 1., 0.], [0., 0., 1.]]
-        for id_row, row in enumerate(mrbbagging._count_votes(x)):
-            for item_id, item in enumerate(row):
-                self.assertEqual(item, expected[id_row][item_id])
-
-    def test__select_classes(self):
-        x = [[1, 1, 1], [2, 2, 2], [3, 3, 3]]
-        with patch.object(MRBBagging, '_count_votes',
-                          return_value=numpy.asarray([[1., 0., 0.], [0., 1., 0.], [0., 0., 1.]])):
-            self.assertEqual(mrbbagging._select_classes(x), ["A", "B", "C"])
+            mrbbagging.fit(x, y, None)
 
 
 if __name__ == '__main__':
