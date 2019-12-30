02d1f8f0becbf19d5e7698c273c02ec86b3ec76c
plutasnyy <kamil.plucinski97@gmail.com>
2019-12-30 15:07:29 +0100

    Fixed counting quantity in soup

---
+++ multi_imbalance/utils/maj_int_min.py
@@ -0,0 +1,21 @@
+maj_int_min = {
+    "1czysty-cut": {'maj': [0], 'int': [2], 'min': [1]},
+    "2delikatne-cut": {'maj': [0], 'int': [2], 'min': [1]},
+    "3mocniej-cut": {'maj': [0], 'int': [2], 'min': [1]},
+    "4delikatne-bezover-cut": {'maj': [0], 'int': [2], 'min': [1]},
+    "balance-scale": {'maj': [2, 1], 'int': [], 'min': [0]},
+    "cleveland": {'maj': [0], 'int': [], 'min': [1, 2, 3, 4]},
+    "cleveland_v2": {'maj': [0], 'int': [], 'min': [1, 2, 3]},
+    "car": {'maj': [2, 0], 'int': [], 'min': [1, 3]},
+    "cmc": {'maj': [0, 2], 'int': [], 'min': [1]},
+    "dermatology": {'maj': [0, 2, 1, 4, 3], 'int': [], 'min': [5]},
+    "flare": {'maj': [1, 2, 3, 6], 'int': [], 'min': [4, 5]},
+    "glass": {'maj': [1, 0, 3], 'int': [], 'min': [5, 2, 4]},
+    "hayes-roth": {'maj': [0, 1], 'int': [], 'min': [2]},
+    "new_ecoli": {'maj': [0, 1], 'int': [], 'min': [4, 2, 3]},
+    "new_led7digit": {'maj': [3, 5, 0, 2], 'int': [], 'min': [4, 1]},
+    "new_vehicle": {'maj': [1], 'int': [], 'min': [0, 2]},
+    "new_winequality-red": {'maj': [0, 1], 'int': [], 'min': [2, 3]},
+    "new_yeast": {'maj': [0, 1, 8, 7], 'int': [], 'min': [6, 5, 4, 3, 2]},
+    "thyroid-newthyroid": {'maj': [0], 'int': [], 'min': [1, 2]}
+}
