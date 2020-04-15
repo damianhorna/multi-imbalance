8c9923ffea700aa19a7d6e6028d27b43c00e2432
damian-horna <damian.horna@interia.pl>
2020-04-15 23:03:11 +0200

    Fix Static-SMOTE tests

--- multi_imbalance/resampling/tests/test_static_smote.py
+++ multi_imbalance/resampling/tests/test_static_smote.py
@@ -1,18 +1,19 @@
-from multi_imbalance.resampling.static_smote import StaticSMOTE
 from collections import Counter
 
-from multi_imbalance.utils.data import load_arff_datasets
+import numpy as np
+
+from multi_imbalance.resampling.static_smote import StaticSMOTE
 
 
 def test_static_smote():
-    datasets = load_arff_datasets()
-    X_ecoli, y_ecoli = datasets['new_ecoli'].data, datasets['new_ecoli'].target
-    ssm = StaticSMOTE()
-    X_resampled, y_resampled = ssm.fit_transform(X_ecoli, y_ecoli)
-    cnt = Counter(y_resampled)
-    assert cnt[0] == 145
-    assert cnt[1] == 77
-    assert cnt[2] == 148
-    assert cnt[3] == 100
-    assert cnt[4] == 104
+    X = np.vstack([np.random.normal(0, 1, (100, 2)),
+                   np.random.normal(3, 5, (30, 2)),
+                   np.random.normal(-2, 2, (20, 2))])
 
+    y = np.array([1] * 100 + [2] * 30 + [3] * 20)
+    ssm = StaticSMOTE()
+    X_resampled, y_resampled = ssm.fit_transform(X, y)
+    cnt = Counter(y_resampled)
+    assert cnt[1] == 100
+    assert cnt[2] == 60
+    assert cnt[3] == 80
