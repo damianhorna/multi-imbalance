b0942e72b87346de24eb834c8ec5261825f2a92c
plutasnyy <kamil.plucinski97@gmail.com>
2020-04-17 22:33:38 +0200

    cleaned requerements

--- multi_imbalance/utils/metrics.py
+++ multi_imbalance/utils/metrics.py
@@ -1,2 +1,5 @@
-def gmean_score():  # pragma no cover
-    pass
+from imblearn.metrics import geometric_mean_score
+
+
+def gmean_score(y_test, y_pred, correction=0.001):  # pragma no cover
+    return geometric_mean_score(y_test, y_pred, correction=correction)
