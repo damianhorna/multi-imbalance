493c4b533ff59f3853ba3fdadab9b0d6eacc1052
damian-horna <damian.horna@interia.pl>
2021-04-29 15:59:50 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setuptools.setup(  # pragma no cover
     ],
     install_requires=[
         "numpy>=1.17.0",
-        "scikit-learn==0.21.3",
+        "scikit-learn>=0.21.3",
         "pandas>=0.25.1",
         "pytest>=5.1.2",
         "imbalanced-learn>=0.6.1",
