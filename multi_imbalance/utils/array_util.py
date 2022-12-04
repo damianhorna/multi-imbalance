b941eb561fb007d2b4c14ffa0d2671ca55d7f537
Maciej Falbogowski <maciej.falbogowski@gmail.com>
2022-12-04 19:55:05 +0100

    Add shuffle function to utils.array_utils which shuffles many arrays at once

--- multi_imbalance/utils/array_util.py
+++ multi_imbalance/utils/array_util.py
@@ -1,4 +1,6 @@
 import numpy as np
+from typing import Optional, Tuple
+import sklearn
 
 
 def setdiff(arr1, arr2):
@@ -83,3 +85,24 @@ def intersect(arr1, arr2):
         if contains(arr2, x):
             result = union(result, np.array([x]))
     return result
+
+
+def shuffle(*arrs: np.ndarray,
+            state: Optional[np.random.RandomState] = None) -> Tuple[
+    np.ndarray, ...]:
+    """
+    Shuffles rows of many arrays at once.
+
+    Shuffles given arrays using a shuffled matrix of row indices.
+    The number of rows in the given arrays should be the same.
+
+    :param arrs:
+        Numpy arrays to shuffle.
+    :param state:
+        Optional RandomState used to shuffle.
+    :return:
+        A tuple of shuffled copies of given arrays.
+    """
+    indices = np.arange(arrs[0].shape[0])
+    sklearn.utils.check_random_state(state).shuffle(indices)
+    return tuple(arr[indices] for arr in arrs)
