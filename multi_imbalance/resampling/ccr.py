5e5113124bc232257f1dfce4b3dc1fbbfb515b74
kot <kozjach@gmail.com>
2023-01-06 22:36:29 +0100

    optimize CCR implementation

--- multi_imbalance/resampling/ccr.py
+++ multi_imbalance/resampling/ccr.py
@@ -17,62 +17,58 @@ class CCR(BaseSampler):
         majority_examples = X[y == 0]
         minority_examples = X[y == 1]
 
+        majority_count = len(majority_examples)
+
         r = np.zeros(minority_examples.shape[0])
-        e = np.full(minority_examples.shape[0], self.energy, dtype=float)
         t = np.zeros(majority_examples.shape)
 
         for i, x in enumerate(minority_examples):
             distances = self.distances(x, majority_examples)
+            sorted_distances_index = np.argsort(distances)
+            energy = self.energy
+            current_example = 0
+            number_of_points_in_radius = 1
 
-            while e[i] > 0:
-                examples_in_radius = distances <= r[i]
-                nop = examples_in_radius.sum() + 1
-                if nop == majority_examples.shape[0]+1:
-                    nop -= 1
-                    dr = e[i]/nop
-                    r[i] += dr
-                    break
+            while current_example != majority_count and energy > 0:
+                example_distance_index = sorted_distances_index[current_example]
+                distance = distances[example_distance_index]
+                if distance <= r[i]:
+                    number_of_points_in_radius += 1
+                dr = energy / number_of_points_in_radius
 
-                dr = e[i] / nop
-                examples_in_radius2 = distances <= r[i] + dr
-                nop2 = examples_in_radius2.sum() + 1
-                if nop2 > nop:
-                    examples_outside_radius = examples_in_radius2 ^ examples_in_radius
-                    outside_index = np.flatnonzero(examples_outside_radius)
-                    newdr = distances[outside_index].min() - r[i]
-                    dr = newdr
+                shortest_distance = distances[sorted_distances_index[current_example]]
+                if r[i] + dr >= shortest_distance:
+                    number_of_points_in_radius += 1
+                    dr = shortest_distance - r[i]
+
                 r[i] += dr
-                e[i] -= dr * ((distances < r[i]).sum() + 1)
+                energy -= dr * (current_example + 1.0)
+                current_example += 1
+
+            if energy > 0:
+                r[i] += energy / (number_of_points_in_radius - 1)
+
             examples_in_range_index = np.flatnonzero(distances <= r[i])
             for j in examples_in_range_index:
                 translation = majority_examples[j] - x
                 d = distances[j]
-                t[j] += (r[i] - d)/d * translation
-                test = t[j]
+                t[j] += (r[i] - d) / d * translation
 
         oversampled_X[y == 0] += t
 
-
-
         G = majority_examples.shape[0] - minority_examples.shape[0]
-        inverse_radius_sum = (r**-1).sum()
+        inverse_radius_sum = (r ** -1).sum()
 
         generated = []
         for i, x in enumerate(minority_examples):
-            g = int(np.round(r[i]**-1/inverse_radius_sum * G))
+            g = int(np.round(r[i] ** -1 / inverse_radius_sum * G))
             for j in range(g):
-                random_translation = np.random.rand(majority_examples.shape[1])*2-1
-                multiplier = random_translation/abs(random_translation).sum()
-                new_point = x+multiplier*r[i]*np.random.rand(1)
+                random_translation = np.random.rand(majority_examples.shape[1]) * 2 - 1
+                multiplier = random_translation / abs(random_translation).sum()
+                new_point = x + multiplier * r[i] * np.random.rand(1)
                 generated.append(new_point)
 
-        return np.concatenate([oversampled_X, generated]), np.concatenate([oversampled_y, [1 for x in generated]]), r
+        return np.concatenate([oversampled_X, generated]), np.concatenate([oversampled_y, [1 for x in generated]])
 
     def distances(self, minority_example, majority_examples):
-        # distances = np.linalg.norm(minority_example - majority_examples, axis=-1, ord=1)
         return (abs(minority_example - majority_examples)).sum(1)
-
-    def NoP(self, minority_example: np.ndarray, majority_examples: np.ndarray, radius: float):
-        # distances = np.linalg.norm(minority_example - majority_examples, axis=-1, ord=1)
-        distances = (abs(minority_example - majority_examples)).sum(1)
-        return (distances < radius).sum() + 1
