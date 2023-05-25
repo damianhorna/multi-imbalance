da9d5cf1ca3e9e73c3c508993e53387ad59ebdbd
Maciej Falbogowski <maciej.falbogowski@gmail.com>
2023-05-25 17:19:41 +0200

    Fix tests in tests/resampling/bracid/test_train_binary.py

--- tests/resampling/bracid/test_train_binary.py
+++ tests/resampling/bracid/test_train_binary.py
@@ -12,7 +12,7 @@ class TestTrain(TestCase):
     def test_train(self):
         """Test with numeric and nominal features"""
         bracid = BRACID()
-        training_set = pd.DataFrame({"A": ["low", "low", "high", "low", "low", "high"], "B": [1, 1, 4, 1.5, 0.5, 0.75],
+        training_set = pd.DataFrame({"B": [1, 1, 4, 1.5, 0.5, 0.75],
                                      "C": [3, 2, 1, .5, 3, 2],
                                      "Class": [_0, _0, _1, _1, _1, _1]})
         # Use majority class as minority to have multiple neighbors and see if the function works correctly
@@ -21,14 +21,17 @@ class TestTrain(TestCase):
         rules = {
             2: pd.Series({"B": Bounds(lower=1.25, upper=4.0), "C": Bounds(lower=0.5, upper=1.5),
                           "Class": _1}, name=2),
-            6: pd.Series({"A": "low", "B": Bounds(lower=0.5, upper=1.5), "C": Bounds(lower=0.5, upper=3.0),
+            6: pd.Series({"B": Bounds(lower=0.5, upper=1.5), "C": Bounds(lower=0.5, upper=3.0),
                           "Class": _1}, name=6),
-            5: pd.Series({"A": "high", "B": Bounds(lower=0.75, upper=4.0), "C": Bounds(lower=1.0, upper=2.5),
+            5: pd.Series({"B": Bounds(lower=0.75, upper=4.0), "C": Bounds(lower=1.0, upper=2.5),
                           "Class": _1}, name=5),
-            0: pd.Series({"A": "low", "B": Bounds(lower=0.5, upper=1.5), "C": Bounds(lower=0.5, upper=3.0),
+            0: pd.Series({"B": Bounds(lower=0.5, upper=1.5), "C": Bounds(lower=0.5, upper=3.0),
                           "Class": _0}, name=0),
         }
         model = bracid.train_binary(rules, training_set, minority_label, class_col_name)
-        correct_model = {2: Support(minority=1.0, majority=0.0), 6: Support(minority=0.5, majority=0.5),
-                         5: Support(minority=1.0, majority=0.0), 0: Support(minority=0.5, majority=0.5)}
-        self.assertEqual(model, correct_model)
+        correct_model = {2: Support(minority=1.0, majority=0.0),
+                         6: Support(minority=0.6, majority=0.4),
+                         5: Support(minority=0.6666666666666666,
+                                    majority=0.33333333333333337),
+                         0: Support(minority=0.6, majority=0.4)}
+        self.assertDictEqual(model, correct_model)
