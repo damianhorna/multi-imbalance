064b42eb15624a4a1ed7e6189f4db3341b89c977
Maciej Falbogowski <maciej.falbogowski@gmail.com>
2023-05-25 14:43:41 +0200

    Fix tests in bracid/test_add_tags.py

--- tests/resampling/bracid/test_add_tags.py
+++ tests/resampling/bracid/test_add_tags.py
@@ -15,35 +15,20 @@ class TestAddTags(TestCase):
 
     def test_add_tags_safe_borderline(self):
         """Add tags when using nominal and numeric features assigning borderline and safe as tags"""
-        df = pd.DataFrame({"A": ["low", "low", "high", "low", "low", "high"], "B": [1, 1, 4, 1.5, 0.5, 0.75],
+        df = pd.DataFrame({"B": [1, 1, 4, 1.5, 0.5, 0.75],
                            "C": [3, 2, 1, .5, 3, 2],
                            "Class": [_0, _0, _1, _1, _1, _1]})
         class_col_name = "Class"
         bracid = BRACID()
-        lookup = \
-            {
-                "A":
-                    {
-                        'high': 2,
-                        'low': 4,
-                        CONDITIONAL:
-                            {
-                                'high':
-                                    Counter({
-                                        _1: 2
-                                    }),
-                                'low':
-                                    Counter({
-                                        _1: 2,
-                                        _0: 2
-                                    })
-                            }
-                    }
-            }
-        correct = pd.DataFrame({"A": ["low", "low", "high", "low", "low", "high"], "B": [1, 1, 4, 1.5, 0.5, 0.75],
+        correct = pd.DataFrame({"B": [1, 1, 4, 1.5, 0.5, 0.75],
                                 "C": [3, 2, 1, .5, 3, 2],
                                 "Class": [_0, _0, _1, _1, _1, _1],
-                                TAG: [ExampleClass.BORDERLINE, ExampleClass.BORDERLINE, ExampleClass.SAFE, ExampleClass.BORDERLINE, ExampleClass.BORDERLINE, ExampleClass.BORDERLINE]
+                                TAG: [ExampleClass.BORDERLINE,
+                                      ExampleClass.BORDERLINE,
+                                      ExampleClass.SAFE,
+                                      ExampleClass.SAFE,
+                                      ExampleClass.BORDERLINE,
+                                      ExampleClass.BORDERLINE]
                                 })
         bracid.closest_rule_per_example = {}
         bracid.closest_examples_per_rule = {}
@@ -55,17 +40,17 @@ class TestAddTags(TestCase):
         min_max = pd.DataFrame({"C": {"min": 1, "max": 5}, "B": {"min": 1, "max": 11}})
         k = 3
         rules = [
-            pd.Series({"A": "low", "B": Bounds(lower=1, upper=1), "C": Bounds(lower=3, upper=3), "Class": _0},
+            pd.Series({"B": Bounds(lower=1, upper=1), "C": Bounds(lower=3, upper=3), "Class": _0},
                       name=0),
-            pd.Series({"A": "low", "B": Bounds(lower=1, upper=1), "C": Bounds(lower=2, upper=2), "Class": _0},
+            pd.Series({"B": Bounds(lower=1, upper=1), "C": Bounds(lower=2, upper=2), "Class": _0},
                       name=1),
-            pd.Series({"A": "high", "B": Bounds(lower=4, upper=4), "C": Bounds(lower=1, upper=1),
+            pd.Series({"B": Bounds(lower=4, upper=4), "C": Bounds(lower=1, upper=1),
                        "Class": _1}, name=2),
-            pd.Series({"A": "low", "B": Bounds(lower=1.5, upper=1.5), "C": Bounds(lower=0.5, upper=0.5),
+            pd.Series({"B": Bounds(lower=1.5, upper=1.5), "C": Bounds(lower=0.5, upper=0.5),
                        "Class": _1}, name=3),
-            pd.Series({"A": "low", "B": Bounds(lower=0.5, upper=0.5), "C": Bounds(lower=3, upper=3),
+            pd.Series({"B": Bounds(lower=0.5, upper=0.5), "C": Bounds(lower=3, upper=3),
                        "Class": _1}, name=4),
-            pd.Series({"A": "high", "B": Bounds(lower=0.75, upper=0.75), "C": Bounds(lower=2, upper=2),
+            pd.Series({"B": Bounds(lower=0.75, upper=0.75), "C": Bounds(lower=2, upper=2),
                        "Class": _1}, name=5)
         ]
         bracid.all_rules = {0: rules[0], 1: rules[1], 2: rules[2], 3: rules[3], 4: rules[4], 5: rules[5]}
@@ -75,32 +60,12 @@ class TestAddTags(TestCase):
 
     def test_add_tags_noisy_safe(self):
         """Add tags when using nominal and numeric features and assigning noisy and safe as tags"""
-        df = pd.DataFrame({"A": ["low", "low", "high", "low", "low", "high"], "B": [1, 1, 4, 1.5, 0.5, 0.75],
+        df = pd.DataFrame({"B": [1, 1, 4, 1.5, 0.5, 0.75],
                            "C": [3, 2, 1, .5, 3, 2],
                            "Class": [_0, _1, _1, _1, _1, _1]})
         class_col_name = "Class"
         bracid = BRACID()
-        lookup = \
-            {
-                "A":
-                    {
-                        'high': 2,
-                        'low': 4,
-                        CONDITIONAL:
-                            {
-                                'high':
-                                    Counter({
-                                        _1: 2
-                                    }),
-                                'low':
-                                    Counter({
-                                        _1: 2,
-                                        _0: 2
-                                    })
-                            }
-                    }
-            }
-        correct = pd.DataFrame({"A": ["low", "low", "high", "low", "low", "high"], "B": [1, 1, 4, 1.5, 0.5, 0.75],
+        correct = pd.DataFrame({"B": [1, 1, 4, 1.5, 0.5, 0.75],
                                 "C": [3, 2, 1, .5, 3, 2],
                                 "Class": [_0, _1, _1, _1, _1, _1],
                                 TAG: [ExampleClass.NOISY, ExampleClass.BORDERLINE, ExampleClass.SAFE, ExampleClass.SAFE, ExampleClass.SAFE, ExampleClass.SAFE]
@@ -115,17 +80,17 @@ class TestAddTags(TestCase):
         min_max = pd.DataFrame({"C": {"min": 1, "max": 5}, "B": {"min": 1, "max": 11}})
         k = 3
         rules = [
-            pd.Series({"A": "low", "B": Bounds(lower=1, upper=1), "C": Bounds(lower=3, upper=3), "Class": _0},
+            pd.Series({"B": Bounds(lower=1, upper=1), "C": Bounds(lower=3, upper=3), "Class": _0},
                       name=0),
-            pd.Series({"A": "low", "B": Bounds(lower=1, upper=1), "C": Bounds(lower=2, upper=2), "Class": _0},
+            pd.Series({"B": Bounds(lower=1, upper=1), "C": Bounds(lower=2, upper=2), "Class": _0},
                       name=1),
-            pd.Series({"A": "high", "B": Bounds(lower=4, upper=4), "C": Bounds(lower=1, upper=1),
+            pd.Series({"B": Bounds(lower=4, upper=4), "C": Bounds(lower=1, upper=1),
                        "Class": _1}, name=2),
-            pd.Series({"A": "low", "B": Bounds(lower=1.5, upper=1.5), "C": Bounds(lower=0.5, upper=0.5),
+            pd.Series({"B": Bounds(lower=1.5, upper=1.5), "C": Bounds(lower=0.5, upper=0.5),
                        "Class": _1}, name=3),
-            pd.Series({"A": "low", "B": Bounds(lower=0.5, upper=0.5), "C": Bounds(lower=3, upper=3),
+            pd.Series({"B": Bounds(lower=0.5, upper=0.5), "C": Bounds(lower=3, upper=3),
                        "Class": _1}, name=4),
-            pd.Series({"A": "high", "B": Bounds(lower=0.75, upper=0.75), "C": Bounds(lower=2, upper=2),
+            pd.Series({"B": Bounds(lower=0.75, upper=0.75), "C": Bounds(lower=2, upper=2),
                        "Class": _1}, name=5)
         ]
         bracid.all_rules = {0: rules[0], 1: rules[1], 2: rules[2], 3: rules[3], 4: rules[4], 5: rules[5]}
@@ -135,52 +100,35 @@ class TestAddTags(TestCase):
 
     def test_add_tags_nan(self):
         """Add tags when using nominal and numeric features when all examples contain at least one NaN value"""
-        df = pd.DataFrame({"A": [np.NaN, np.NaN, "high", np.NaN, "low", np.NaN],
-                           "B": [np.NaN, 1, np.NaN, 1.5, np.NaN, 0.75],
+        df = pd.DataFrame({"B": [np.NaN, 1, np.NaN, 1.5, np.NaN, 0.75],
                            "C": [3, 2, 1, .5, 3, 2],
                            "Class": [_0, _0, _1, _1, _1, _1]})
         bracid = BRACID()
         bracid.examples_covered_by_rule = {0: {0}, 1: {1}, 2: {2}, 3: {3}, 4: {4}, 5: {5}}
         class_col_name = "Class"
-        lookup = \
-            {
-                "A":
-                    {
-                        'high': 1,
-                        'low': 1,
-                        CONDITIONAL:
-                            {
-                                'high':
-                                    Counter({
-                                        _1: 1
-                                    }),
-                                'low':
-                                    Counter({
-                                        _1: 1
-                                    })
-                            }
-                    }
-            }
-        correct = pd.DataFrame({"A": [np.NaN, np.NaN, "high", np.NaN, "low", np.NaN],
-                                "B": [np.NaN, 1, np.NaN, 1.5, np.NaN, 0.75],
-                                "C": [3, 2, 1, .5, 3, 2],
-                                "Class": [_0, _0, _1, _1, _1, _1],
-                                TAG: [ExampleClass.BORDERLINE, ExampleClass.NOISY, ExampleClass.SAFE, ExampleClass.SAFE, ExampleClass.SAFE, ExampleClass.SAFE]
-                                })
+        correct = pd.DataFrame(
+            {"B": [np.NaN, 1, np.NaN, 1.5, np.NaN, 0.75],
+             "C": [3, 2, 1, .5, 3, 2],
+             "Class": [_0, _0, _1, _1, _1, _1],
+             TAG: [ExampleClass.BORDERLINE, ExampleClass.BORDERLINE,
+                   ExampleClass.SAFE, ExampleClass.SAFE, ExampleClass.SAFE,
+                   ExampleClass.BORDERLINE]
+             })
+
         classes = [_0, _1]
         min_max = pd.DataFrame({"C": {"min": 1, "max": 5}, "B": {"min": 1, "max": 11}})
         rules = [
-            pd.Series({"A": "low", "B": Bounds(lower=1, upper=1), "C": Bounds(lower=3, upper=3), "Class": _0},
+            pd.Series({"B": Bounds(lower=1, upper=1), "C": Bounds(lower=3, upper=3), "Class": _0},
                       name=0),
-            pd.Series({"A": "low", "B": Bounds(lower=1, upper=1), "C": Bounds(lower=2, upper=2), "Class": _0},
+            pd.Series({"B": Bounds(lower=1, upper=1), "C": Bounds(lower=2, upper=2), "Class": _0},
                       name=1),
-            pd.Series({"A": "high", "B": Bounds(lower=4, upper=4), "C": Bounds(lower=1, upper=1),
+            pd.Series({"B": Bounds(lower=4, upper=4), "C": Bounds(lower=1, upper=1),
                        "Class": _1}, name=2),
-            pd.Series({"A": "low", "B": Bounds(lower=1.5, upper=1.5), "C": Bounds(lower=0.5, upper=0.5),
+            pd.Series({"B": Bounds(lower=1.5, upper=1.5), "C": Bounds(lower=0.5, upper=0.5),
                        "Class": _1}, name=3),
-            pd.Series({"A": "low", "B": Bounds(lower=0.5, upper=0.5), "C": Bounds(lower=3, upper=3),
+            pd.Series({"B": Bounds(lower=0.5, upper=0.5), "C": Bounds(lower=3, upper=3),
                        "Class": _1}, name=4),
-            pd.Series({"A": "high", "B": Bounds(lower=0.75, upper=0.75), "C": Bounds(lower=2, upper=2),
+            pd.Series({"B": Bounds(lower=0.75, upper=0.75), "C": Bounds(lower=2, upper=2),
                        "Class": _1}, name=5)
         ]
         k = 3
@@ -198,53 +146,39 @@ class TestAddTags(TestCase):
     def test_add_tags_all_tags(self):
         """Add tags when using nominal and numeric features and assigning noisy, borderline and safe as tags"""
         bracid = BRACID()
-        df = pd.DataFrame({"A": ["low", "low", "high", "low", "low", "high"], "B": [1, 1, 4, 1.5, 0.5, 0.75],
+        df = pd.DataFrame({"B": [1, 1, 4, 1.5, 0.5, 0.75],
                            "C": [3, 2, 1, .5, 3, 2],
                            "Class": [_0, _0, _1, _1, _1, _1]})
         class_col_name = "Class"
         bracid.examples_covered_by_rule = {0: {0}, 1: {1}, 2: {2}, 3: {3}, 4: {4}, 5: {5}}
-        lookup = \
-            {
-                "A":
-                    {
-                        'high': 2,
-                        'low': 4,
-                        CONDITIONAL:
-                            {
-                                'high':
-                                    Counter({
-                                        _1: 2
-                                    }),
-                                'low':
-                                    Counter({
-                                        _1: 2,
-                                        _0: 2
-                                    })
-                            }
-                    }
-            }
-        correct = pd.DataFrame({"A": ["low", "low", "high", "low", "low", "high"], "B": [1, 1, 4, 1.5, 0.5, 0.75],
-                                "C": [3, 2, 1, .5, 3, 2],
-                                "Class": [_0, _0, _1, _1, _1, _1],
-                                TAG: [ExampleClass.BORDERLINE, ExampleClass.BORDERLINE, ExampleClass.SAFE, ExampleClass.NOISY, ExampleClass.NOISY, ExampleClass.BORDERLINE]
-                                })
+        correct = pd.DataFrame({
+            "B": [1, 1, 4, 1.5, 0.5, 0.75],
+            "C": [3, 2, 1, 0.5, 3, 2],
+            "Class": [_0, _0, _1, _1, _1, _1],
+            TAG: [ExampleClass.BORDERLINE, ExampleClass.BORDERLINE,
+                  ExampleClass.BORDERLINE, ExampleClass.BORDERLINE,
+                  ExampleClass.BORDERLINE, ExampleClass.NOISY]
+        })
+
         classes = [_0, _1]
         min_max = pd.DataFrame({"C": {"min": 1, "max": 5}, "B": {"min": 1, "max": 11}})
         k = 2
         rules = [
-            pd.Series({"A": "low", "B": Bounds(lower=1, upper=1), "C": Bounds(lower=3, upper=3), "Class": _0},
+            pd.Series({"B": Bounds(lower=1, upper=1), "C": Bounds(lower=3, upper=3), "Class": _0},
                       name=0),
-            pd.Series({"A": "low", "B": Bounds(lower=1, upper=1), "C": Bounds(lower=2, upper=2), "Class": _0},
+            pd.Series({"B": Bounds(lower=1, upper=1), "C": Bounds(lower=2, upper=2), "Class": _0},
                       name=1),
-            pd.Series({"A": "high", "B": Bounds(lower=4, upper=4), "C": Bounds(lower=1, upper=1),
+            pd.Series({"B": Bounds(lower=4, upper=4), "C": Bounds(lower=1, upper=1),
                        "Class": _1}, name=2),
-            pd.Series({"A": "low", "B": Bounds(lower=1.5, upper=1.5), "C": Bounds(lower=0.5, upper=0.5),
+            pd.Series({"B": Bounds(lower=1.5, upper=1.5), "C": Bounds(lower=0.5, upper=0.5),
                        "Class": _1}, name=3),
-            pd.Series({"A": "low", "B": Bounds(lower=0.5, upper=0.5), "C": Bounds(lower=3, upper=3),
+            pd.Series({"B": Bounds(lower=0.5, upper=0.5), "C": Bounds(lower=3, upper=3),
                        "Class": _1}, name=4),
-            pd.Series({"A": "high", "B": Bounds(lower=0.75, upper=0.75), "C": Bounds(lower=2, upper=2),
+            pd.Series({"B": Bounds(lower=0.75, upper=0.75), "C": Bounds(lower=2, upper=2),
                        "Class": _1}, name=5)
         ]
+
+
         bracid.all_rules = {0: rules[0], 1: rules[1], 2: rules[2], 3: rules[3], 4: rules[4], 5: rules[5]}
         bracid.closest_rule_per_example = {}
         bracid.closest_examples_per_rule = {}
