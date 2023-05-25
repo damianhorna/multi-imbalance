cacd80a26500c77de409d9a9fa59a2965fbf04fc
Maciej Falbogowski <maciej.falbogowski@gmail.com>
2023-05-25 16:44:29 +0200

    Fix tests in test_evaluate_f1_update_confusion_matrix.py

--- tests/resampling/bracid/test_evaluate_f1_update_confusion_matrix.py
+++ tests/resampling/bracid/test_evaluate_f1_update_confusion_matrix.py
@@ -1,11 +1,10 @@
 from unittest import TestCase
-from collections import Counter
 
 import pandas as pd
 
 from multi_imbalance.resampling.bracid.bracid import BRACID, Data, Bounds, ConfusionMatrix
-import multi_imbalance.resampling.bracid.vars as my_vars
 from tests.resampling.bracid.classes_ import _0, _1
+from tests.resampling.bracid.assertions import assert_almost_equal
 
 
 class TestEvaluateF1UpdateConfusionMatrix(TestCase):
@@ -15,46 +14,26 @@ class TestEvaluateF1UpdateConfusionMatrix(TestCase):
         """Tests what happens if input has a numeric and a nominal feature and a rule that predicts an example is
         updated"""
         bracid = BRACID()
-        df = pd.DataFrame({"A": ["low", "low", "high", "low", "low", "high"], "B": [1, 1, 4, 1.5, 0.5, 0.75],
+        df = pd.DataFrame({"B": [1, 1, 4, 1.5, 0.5, 0.75],
                            "C": [3, 2, 1, .5, 3, 2],
                            "Class": [_0, _0, _1, _1, _1, _1]})
         class_col_name = "Class"
-        lookup = \
-            {
-                "A":
-                    {
-                        'high': 2,
-                        'low': 4,
-                        my_vars.CONDITIONAL:
-                            {
-                                'high':
-                                    Counter({
-                                        _1: 2
-                                    }),
-                                'low':
-                                    Counter({
-                                        _1: 2,
-                                        _0: 2
-                                    })
-                            }
-                    }
-            }
         classes = [_0, _1]
         min_max = pd.DataFrame({"B": {"min": 1, "max": 5}, "C": {"min": 1, "max": 11}})
         bracid.minority_class = _0
 
         rules = [
-            pd.Series({"A": "low", "B": Bounds(lower=1, upper=1), "C": Bounds(lower=3, upper=3), "Class": _0},
+            pd.Series({"B": Bounds(lower=1, upper=1), "C": Bounds(lower=3, upper=3), "Class": _0},
                       name=0),
-            pd.Series({"A": "low", "B": Bounds(lower=1, upper=1), "C": Bounds(lower=2, upper=2), "Class": _0},
+            pd.Series({"B": Bounds(lower=1, upper=1), "C": Bounds(lower=2, upper=2), "Class": _0},
                       name=1),
-            pd.Series({"A": "high", "B": Bounds(lower=4, upper=4), "C": Bounds(lower=1, upper=1),
+            pd.Series({"B": Bounds(lower=4, upper=4), "C": Bounds(lower=1, upper=1),
                        "Class": _1}, name=2),
-            pd.Series({"A": "low", "B": Bounds(lower=1.5, upper=1.5), "C": Bounds(lower=0.5, upper=0.5),
+            pd.Series({"B": Bounds(lower=1.5, upper=1.5), "C": Bounds(lower=0.5, upper=0.5),
                        "Class": _1}, name=3),
-            pd.Series({"A": "low", "B": Bounds(lower=0.5, upper=0.5), "C": Bounds(lower=3, upper=3),
+            pd.Series({"B": Bounds(lower=0.5, upper=0.5), "C": Bounds(lower=3, upper=3),
                        "Class": _1}, name=4),
-            pd.Series({"A": "high", "B": Bounds(lower=0.75, upper=0.75), "C": Bounds(lower=2, upper=2),
+            pd.Series({"B": Bounds(lower=0.75, upper=0.75), "C": Bounds(lower=2, upper=2),
                        "Class": _1}, name=5)
         ]
         bracid.closest_rule_per_example = {
@@ -70,7 +49,7 @@ class TestEvaluateF1UpdateConfusionMatrix(TestCase):
         bracid.seed_example_rule = {0: {0}, 1: {1}, 2: {2}, 3: {3}, 4: {4}, 5: {5}}
         bracid.all_rules = {0: rules[0], 1: rules[1], 2: rules[2], 3: rules[3], 4: rules[4], 5: rules[5]}
         bracid.conf_matrix = ConfusionMatrix(TP= {0, 1}, FP= {3, 4}, TN= {2, 5}, FN= set())
-        new_rule = pd.Series({"A": "low", "B": Bounds(lower=0.5, upper=1.0), "C": Bounds(lower=3, upper=3),
+        new_rule = pd.Series({"B": Bounds(lower=0.5, upper=1.0), "C": Bounds(lower=3, upper=3),
                               "Class": _1}, name=0)
         # tagged, initial_rules = add_tags_and_extract_rules(df, 2, class_col_name, min_max, classes)
         correct_f1 = 0.8
@@ -78,63 +57,39 @@ class TestEvaluateF1UpdateConfusionMatrix(TestCase):
         correct_closest_rule_per_example = {
             0: Data(rule_id=1, dist=0.010000000000000002),
             1: Data(rule_id=0, dist=0.010000000000000002),
-            2: Data(rule_id=5, dist=0.67015625),
-            3: Data(rule_id=1, dist=0.038125),
+            2: Data(rule_id=0, dist=0.6025), 3: Data(rule_id=1, dist=0.038125),
             4: Data(rule_id=0, dist=0.0),
-            5: Data(rule_id=2, dist=0.67015625)}
+            5: Data(rule_id=0, dist=0.010000000000000002)}
         print(f1, correct_f1)
-        self.assertEqual(f1, correct_f1)
-        for example_id in bracid.closest_rule_per_example:
-            rule_id, dist = bracid.closest_rule_per_example[example_id]
-            self.assertTrue(rule_id == correct_closest_rule_per_example[example_id][0] and
-                            abs(dist - correct_closest_rule_per_example[example_id][1]) < 0.001)
-        correct_conf_matrix = ConfusionMatrix(TP= {0, 1}, FP= {3}, TN= {2, 4, 5}, FN= set())
+        self.assertAlmostEqual(f1, correct_f1)
+        assert_almost_equal(self, bracid.closest_rule_per_example, correct_closest_rule_per_example)
+        correct_conf_matrix = ConfusionMatrix(TP={0, 1}, TN={2, 4, 5}, FP={3}, FN=set())
         self.assertEqual(bracid.conf_matrix, correct_conf_matrix)
 
     def test_evaluate_f1_update_confusion_matrix_not_updated(self):
         """Tests what happens if input has a numeric and a nominal feature and a rule that predicts an example is
         not updated as F1 score doesn't improve"""
         bracid = BRACID()
-        df = pd.DataFrame({"A": ["low", "low", "high", "low", "low", "high"], "B": [1, 1, 4, 1.5, 0.5, 0.75],
+        df = pd.DataFrame({"B": [1, 1, 4, 1.5, 0.5, 0.75],
                            "C": [3, 2, 1, .5, 3, 2],
                            "Class": [_0, _0, _1, _1, _1, _1]})
         class_col_name = "Class"
-        lookup = \
-            {
-                "A":
-                    {
-                        'high': 2,
-                        'low': 4,
-                        my_vars.CONDITIONAL:
-                            {
-                                'high':
-                                    Counter({
-                                        _1: 2
-                                    }),
-                                'low':
-                                    Counter({
-                                        _1: 2,
-                                        _0: 2
-                                    })
-                            }
-                    }
-            }
         classes = [_0, _1]
         min_max = pd.DataFrame({"B": {"min": 1, "max": 5}, "C": {"min": 1, "max": 11}})
         bracid.minority_class = _0
 
         rules = [
-            pd.Series({"A": "low", "B": Bounds(lower=1, upper=1), "C": Bounds(lower=3, upper=3), "Class": _0},
+            pd.Series({"B": Bounds(lower=1, upper=1), "C": Bounds(lower=3, upper=3), "Class": _0},
                       name=0),
-            pd.Series({"A": "low", "B": Bounds(lower=1, upper=1), "C": Bounds(lower=2, upper=2), "Class": _0},
+            pd.Series({"B": Bounds(lower=1, upper=1), "C": Bounds(lower=2, upper=2), "Class": _0},
                       name=1),
-            pd.Series({"A": "high", "B": Bounds(lower=4, upper=4), "C": Bounds(lower=1, upper=1),
+            pd.Series({"B": Bounds(lower=4, upper=4), "C": Bounds(lower=1, upper=1),
                        "Class": _1}, name=2),
-            pd.Series({"A": "low", "B": Bounds(lower=1.5, upper=1.5), "C": Bounds(lower=0.5, upper=0.5),
+            pd.Series({"B": Bounds(lower=1.5, upper=1.5), "C": Bounds(lower=0.5, upper=0.5),
                        "Class": _1}, name=3),
-            pd.Series({"A": "low", "B": Bounds(lower=0.5, upper=0.5), "C": Bounds(lower=3, upper=3),
+            pd.Series({"B": Bounds(lower=0.5, upper=0.5), "C": Bounds(lower=3, upper=3),
                        "Class": _1}, name=4),
-            pd.Series({"A": "high", "B": Bounds(lower=0.75, upper=0.75), "C": Bounds(lower=2, upper=2),
+            pd.Series({"B": Bounds(lower=0.75, upper=0.75), "C": Bounds(lower=2, upper=2),
                        "Class": _1}, name=5)
         ]
         bracid.examples_covered_by_rule = {}
@@ -150,7 +105,7 @@ class TestEvaluateF1UpdateConfusionMatrix(TestCase):
             4: Data(rule_id=0, dist=0.015625),
             5: Data(rule_id=2, dist=0.67015625)}
         bracid.conf_matrix = ConfusionMatrix(TP= {0, 1}, FP= set(), TN= {2, 5}, FN= {3, 4})
-        new_rule = pd.Series({"A": "low", "B": (0.5, 0.5), "C": (3, 3), "Class": _1}, name=4)
+        new_rule = pd.Series({"B": (0.5, 0.5), "C": (3, 3), "Class": _1}, name=4)
         correct_f1 = 2*1*0.5/1.5
 
         f1 = bracid.evaluate_f1_update_confusion_matrix(df, new_rule, class_col_name, min_max, classes)
@@ -160,11 +115,8 @@ class TestEvaluateF1UpdateConfusionMatrix(TestCase):
             2: Data(rule_id=5, dist=0.67015625),
             3: Data(rule_id=1, dist=0.038125),
             4: Data(rule_id=0, dist=0.015625),
-            5: Data(rule_id=2, dist=0.67015625)}
-        self.assertEqual(f1, correct_f1)
-        for example_id in bracid.closest_rule_per_example:
-            rule_id, dist = bracid.closest_rule_per_example[example_id]
-            self.assertTrue(rule_id == correct_closest_rule_per_example[example_id][0] and
-                            abs(dist - correct_closest_rule_per_example[example_id][1]) < 0.001)
-        correct_conf_matrix = ConfusionMatrix(TP= {0, 1}, FP= set(), TN= {2, 5}, FN= {3, 4})
+            5: Data(rule_id=4, dist=0.013906250000000002)}
+        self.assertAlmostEqual(f1, correct_f1)
+        assert_almost_equal(self, bracid.closest_rule_per_example, correct_closest_rule_per_example)
+        correct_conf_matrix = ConfusionMatrix(TP={0, 1}, TN={2, 5}, FP=set(), FN={3, 4})
         self.assertEqual(bracid.conf_matrix, correct_conf_matrix)
