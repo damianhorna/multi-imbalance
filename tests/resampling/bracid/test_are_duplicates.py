c636642590264dff4c94bb35916fdfbce4fea08f
Maciej Falbogowski <maciej.falbogowski@gmail.com>
2023-05-25 15:05:59 +0200

    Fix tests in tests/resampling/bracid/test_are_duplicates.py

--- tests/resampling/bracid/test_are_duplicates.py
+++ tests/resampling/bracid/test_are_duplicates.py
@@ -13,9 +13,9 @@ class TestAreDuplicates(TestCase):
         """Tests that no duplicate rules are detected if they are different in a lower or upper boundary value"""
         bracid = BRACID()
         rules = [
-            pd.Series({"A": "high", "B": Bounds(lower=1, upper=1), "C": Bounds(lower=1, upper=1), "Class": _0},
+            pd.Series({"B": Bounds(lower=1, upper=1), "C": Bounds(lower=1, upper=1), "Class": _0},
                       name=1),
-            pd.Series({"A": "high", "B": Bounds(lower=0.8, upper=1), "C": Bounds(lower=1, upper=1),
+            pd.Series({"B": Bounds(lower=0.8, upper=1), "C": Bounds(lower=1, upper=1),
                        "Class": _0}, name=2)
             ]
         duplicate = bracid._are_duplicates(rules[0], rules[1])
@@ -25,9 +25,9 @@ class TestAreDuplicates(TestCase):
         """Tests that two rules are detected as duplicates if only the rule ID is different in both rules"""
         bracid = BRACID()
         rules = [
-            pd.Series({"A": "high", "B": Bounds(lower=1, upper=1), "C": Bounds(lower=1, upper=1), "Class": _0},
+            pd.Series({"B": Bounds(lower=1, upper=1), "C": Bounds(lower=1, upper=1), "Class": _0},
                       name=1),
-            pd.Series({"A": "high", "B": Bounds(lower=1, upper=1), "C": Bounds(lower=1, upper=1),
+            pd.Series({"B": Bounds(lower=1, upper=1), "C": Bounds(lower=1, upper=1),
                        "Class": _0}, name=2)
             ]
         duplicate = bracid._are_duplicates(rules[0], rules[1])
@@ -37,10 +37,9 @@ class TestAreDuplicates(TestCase):
         """Tests that two rules of different lengths can never be duplicates"""
         bracid = BRACID()
         rules = [
-            pd.Series({"A": "high", "B": Bounds(lower=1, upper=1), "C": Bounds(lower=1, upper=1), "Class": _0},
+            pd.Series({"B": Bounds(lower=1, upper=1), "C": Bounds(lower=1, upper=1), "Class": _0},
                       name=1),
-            pd.Series({"B": Bounds(lower=1, upper=1), "C": Bounds(lower=1, upper=1),
-                       "Class": _0}, name=2)
+            pd.Series({"B": Bounds(lower=1, upper=1), "Class": _0}, name=2)
             ]
         duplicate = bracid._are_duplicates(rules[0], rules[1])
         self.assertFalse(duplicate)
